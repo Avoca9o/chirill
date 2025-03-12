7ada1edbb3f4865c58c79a913ee74d80b450e6fb
Avoca9o <davladimirov@edu.hse.ru>
2025-03-12 11:36:03 +0300

    add tramp

--- frontend/src/Video.js
+++ frontend/src/Video.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import videoFile from './resources/mask.mp4';
+import videoFile2 from './resources/tramp.mp4';
 import congratulationsFile from './resources/congratulations.png';
 
 import "./counters.css";
@@ -64,6 +65,49 @@ const Video = () => {
           <div className="counters">
             <div>Набрано<br /> {count}</div>
             <div>Цель<br /> {aim}</div>
+          </div>
+        </div>
+      ) : count < aim2 ? (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center', // вертикально по центру
+          alignItems: 'center',     // горизонтально по центру
+          height: '100vh',          // по высоте занимаем весь экран устройства
+          width: '100%',            // используем всю ширину
+          textAlign: 'center'
+        }}>
+          <video
+            ref={videoRef}
+            src={videoFile2}
+            width="auto"
+            height="70%"
+            playsInline
+            webkit-playsinline="true"
+            controls={false}
+            preload="auto"
+          />
+
+          <button
+            onClick={handlePlay}
+            style={{
+              padding: '10px 20px',
+              fontSize: '18px',
+              backgroundColor: '#007bff',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              marginTop: '20px',
+              transition: 'transform 0.2s ease-in-out',
+            }}
+          >
+            Chinazes
+          </button>
+
+          <div className="counters">
+            <div>Набрано<br /> {count}</div>
+            <div>Цель<br /> {aim2}</div>
           </div>
         </div>
       ) : (
